5fb787a9c3424c4da14cebb4436b0512d9d372a9
Kim Palao <kimfarhantpalao@gmail.com>
2023-11-21 22:49:08 +0800

    Customise AudioAdmin

--- api/admin.py
+++ api/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Audio
 
+class AudioAdmin(admin.ModelAdmin):
+    list_display = 'name', 'explicit'
+
 # Register your models here.
-admin.site.register(Audio)
+admin.site.register(Audio, AudioAdmin)
