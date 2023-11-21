f064b529c43bfea65283cc8bd071e128012fe34f
Kim Palao <kimfarhantpalao@gmail.com>
2023-11-21 22:49:08 +0800

    Add link to frontend audio

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-
+from os import environ
 from .models import Audio
 
 class AudioAdmin(admin.ModelAdmin):
     list_display = 'name', 'explicit'
 
     prepopulated_fields = {"slug": ["name"]}
+
+    def render_change_form(self, request, context, *args, **kwargs):
+        self.change_form_template = 'admin/api/audio_change_form_with_frontend_link.html'
+        context['frontend_url'] = environ["FRONTEND_URL"]
+        return super().render_change_form(request, context, *args, **kwargs)
+
 
 
 # Register your models here.
